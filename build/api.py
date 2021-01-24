c444c572163cb71b01aa940c65dfeb998c559ab6
JTaraba <jwtaraba@gmail.com>
2021-01-24 01:35:48 -0500

    functional devices table in the database
    Test file would need to add data to the database through a dictionary but the api seems to be working for get put and patch requests to the devices table

--- build/api.py
+++ build/api.py
@@ -1,33 +1,32 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.ext.declarative import declarative_base
 
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 
 class DeviceModel(db.Model):
     __tablename__ = 'devices'
-    deviceID = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(20), nullable = False)
-    device_type = db.Column(db.Integer, nullable = False)
-    edgeID = db.Column(db.Integer, db.ForeignKey('edgestations.edgeID'), nullable = False)
-    edgestations = db.relationship('EdgeStationsModel', backref = db.backref('posts', lazy = True))
+    name = db.Column(db.String(20), nullable = False, unique = True)
+    IDdevice = db.Column(db.Integer, primary_key = True)
+    device_type = db.Column(db.String(20), nullable = False)
+    #edgeID = db.Column(db.Integer, db.ForeignKey('edgestations.edgeID'), nullable = False)
+    #edgestations = db.relationship('EdgeStationsModel', backref = db.backref('posts', lazy = True))
 
 
     def __repr__(self):
-        return f"Devices(name ={name}, deviceID = {deviceID}, device_type = {device_type}) " 
-
+        return f"Devices(name ={name}, deviceID = {IDdevice}, device_type = {device_type}) " 
+'''
 class EdgeStationsModel(db.Model):
     __tablename__ = 'edge_stations'
     edgeID = db.Column(db.Integer, primary_key = True)
     edge_name = db.Column(db.String(20), nullable = False)
     edge_ip = db.Column(db.String(15), nullable = False)
     edge_port = db.Column(db.Integer, nullable = False)
-'''
+
 class ReadingsModel(db.Model):
     __tablename__ = 'readings'
     applicationID = db.Column(db.Integer, primary_key = True)
@@ -41,40 +40,64 @@ class PredictionsModel(db.Model):
     applicationID = db.Column(db.Integer, primary_key = True)
 '''
 
-db.create_all()
-
 devices_put_args = reqparse.RequestParser()
 devices_put_args.add_argument("name", type=str, help="Name of device is required", required=True)
 devices_put_args.add_argument("device_type", type=str, help="Type of device is required", required=True)
 devices_put_args.add_argument("deviceID", type=int, help="ID of device is required", required=True)
 
+device_update_args = reqparse.RequestParser()
+device_update_args.add_argument("name", type=str, help="Name of device is required")
+device_update_args.add_argument("device_type", type=str, help="Type of device is required")
+
 
 
 device_fields = {
-    'deviceID' : fields.Integer,
+    'IDdevice' : fields.Integer,
     'name' : fields.String,
-    'device_type': fields.String,
-    'edgeID': fields.Integer
+    'device_type': fields.String
 }
 
-class Devices():
+class Devices(Resource):
     @marshal_with(device_fields)
     def get(self, device_id):
-        result = DeviceModel.query.get(deviceID = device_id)
+        result = DeviceModel.query.filter_by(IDdevice = device_id).first()
+        if not result:
+            abort(404, message = "Could not find device")
         return result
         
     @marshal_with(device_fields)
     def put(self, device_id):
         args = devices_put_args.parse_args()
-        device = DeviceModel(deviceID = device_id, name = args['name'], device_type = args['device_type'], edgeID = args['edgeID'])
+        result = DeviceModel.query.filter_by(IDdevice = device_id).first()
+        if result:
+            abort(409, message = "Device ID exists")
+        device = DeviceModel(name = args['name'], IDdevice = device_id, device_type = args['device_type'])
         db.session.add(device)
         db.session.commit()
         return device, 201
+    
+    @marshal_with(device_fields)
+    def patch(self, device_id):
+        args = device_update_args.parse_args()
+        result = DeviceModel.query.filter_by(IDdevice = device_id).first()
+        if not result:
+            abort(404, message = "No such device, update failed") 
+        if args['name']:
+            result.name = args['name']
+        if args['device_type']:
+            result.device_type = args['device_type']
+
+        db.session.commit()
+
+        return result
+
+
 '''
 class EdgeStation():
     def get(self, edgestation_id):
         result = EdgeStationsModel.query.get(edgestation_id = edgestation_id)
 '''
+api.add_resource(Devices, '/devices/<int:device_id>')
 
 
 if __name__ == "__main__":
